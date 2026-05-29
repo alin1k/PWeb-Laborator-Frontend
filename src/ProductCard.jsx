9d8bbb99b317e8d999db63b453ea4395b4553b92
alin <alin@delipal.ro>
2026-05-29 23:02:39 +0300

    [LAB10-EX1] Add Pret component

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,10 +1,13 @@
+import Pret from "./Pret.jsx";
 import StockBadge from "./StockBadge.jsx";
 
 function ProductCard({ product, onSelect }) {
   return (
     <div className="product-card">
       <h3>{product.name}</h3>
-      <div className="product-price">{product.price.toFixed(2)} lei</div>
+      <div className="product-price">
+        <Pret valoare={product.price} />
+      </div>
       <div className="product-stock">
         Stoc: {product.stock} buc
         <StockBadge stock={product.stock} />
